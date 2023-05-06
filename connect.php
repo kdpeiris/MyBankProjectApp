e5c53f7e014c6cde3c7f1a8483604749f71e9650
Dilshan Peiris <dilshanpeiris51@gmail.com>
2023-05-06 09:38:10 +0200

    Implementing PHP Web App

---
+++ connect.php
@@ -0,0 +1,36 @@
+<?php
+    if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])){
+        //$conn = mysqli_connect('localhost', 'root', '', 'myproject01db') or die("Connection Failed:" .mysqli_connect_error());
+        $conn = mysqli_init();
+        //mysqli_ssl_set($conn,NULL,NULL, "{path to CA cert}", NULL, NULL);
+        mysqli_real_connect($conn, "mybankprojectdbserver.mysql.database.azure.com", "kdpeiris", "Dil@0718", "mybankprojectdb", 3306);
+        if(mysqli_connect_errno($conn)){
+            die('Failed to connect to MySQL: '.mysqli_connect_error());
+        }
+        if(isset($_POST['gname']) && isset($_POST['sname']) && isset($_POST['bday']) && isset($_POST['nic']) && isset($_POST['gender']) && isset($_POST['email']) && isset($_POST['pnumber']) && isset($_POST['atype'])){
+            $given_name = $_POST['gname'];
+            $surname = $_POST['sname'];
+            $dob = $_POST['bday'];
+            $nic = $_POST['nic'];
+            $gender = $_POST['gender'];
+            $email = $_POST['email'];
+            $phone_no = $_POST['pnumber'];
+            $account_type = $_POST['atype'];
+
+            $sql = "INSERT INTO customers (given_name, surname, dob, nic, gender, email, phone_no, account_type) VALUES ('$given_name', '$surname', '$dob', '$nic', ' $gender', '$email', '$phone_no', '$account_type')";
+            $query = mysqli_query($conn,$sql);
+
+            if($query){
+                echo "Entry Successful";
+                echo "&nbsp; &nbsp; &nbsp; <a href='index.php'>Add another record</a>";
+            }
+            else{
+                echo "Error Occured";
+                echo "&nbsp; &nbsp; &nbsp; <a href='index.php'>Back</a>";
+            }
+        }
+        
+    }
+
+
+?>
---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Customer Onboarding</title>
+</head>
+<body>
+    
+    <h1 style="font-family:verdana;"> Customer Onboarding </h1>
+    <form action="connect.php" method="post">
+        <table>
+            <tr>
+                <td><label for="labelgname">Given Name:</label></td>
+                <td><input type="text" id="gname" name="gname" required></td>
+            </tr>
+            <tr>
+                <td><label for="labelsname">Surname:</label></td>
+                <td><input type="text" id="sname" name="sname" required></td>
+            </tr>
+            <tr>
+                <td><label for="labelbday">Date of Birth:</label></td>
+                <td><input type="date" id="bday" name="bday" required></td>
+            </tr>
+            <tr>
+                <td><label for="labelnic">NIC:</label></td>
+                <td><input type="text" id="nic" name="nic" required></td>
+            </tr>
+            <tr>
+                <td><label for="labelgender">Gender:</label></td>
+                <td>
+                    <select name="gender" id="gender">
+                        <option value="male" selected="selected">Male</option>
+                        <option value="female">Female</option>
+                        <option value="intersex">Intersex</option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td><label for="labelemail">Email:</label></td>
+                <td><input type="email" id="email" name="email"></td>
+            </tr>
+            <tr>
+                <td><label for="labelpnumber">Phone Number:</label></td>
+                <td><input type="text" id="pnumber" name="pnumber" required></td>
+            </tr>
+            <tr>
+                <td><label for="labelatype">Account Type:</label></td>
+                <td>
+                    <select name="atype" id="atype">
+                        <option value="savings" selected="selected">Savings</option>
+                        <option value="savingsplus">Savings Plus</option>
+                        <option value="current">Current</option>
+                    </select>
+                </td>
+            </tr>
+            <tr></tr>
+            <tr>
+                <td>
+                    <input type="submit" name="submit" id="submit" value="Submit">
+                    <input type="reset" name="reset" id="reset" value="Reset">
+                </td>
+            </tr>
+        </table>
+    </form>
+    
+</body>
+</html>
